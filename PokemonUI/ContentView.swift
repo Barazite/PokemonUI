52ee7cba0b9f69387efbc4c48659b2ef8983180f
Adrian <adrian.sevilla.diaz@gmail.com>
2022-01-17 10:22:56 +0100

    Initial commit

---
+++ PokemonUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokemonUI
+//
+//  Created by Adrian Sevilla Diaz on 17/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokemonUI/PokemonUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonUIApp.swift
+//  PokemonUI
+//
+//  Created by Adrian Sevilla Diaz on 17/1/22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
